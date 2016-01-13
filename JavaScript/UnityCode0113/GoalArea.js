0f09f80cdd09f638ac1b11a846e21c945b89312e
gab0626 <kyhk.0626@outlook.jp>
2016-01-13 23:06:15 +0900

    0113

---
+++ JavaScript/UnityCode0113/GoalArea.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+public static var goal : boolean;
+
+function Start () {
+	goal = false;
+}
+
+function Update () {
+
+}
+
+function OnTriggerEnter(col : Collider){
+	
+	if(col.gameObject.tag == "Player"){
+		goal = true;
+	}
+
+}
---
+++ JavaScript/UnityCode0113/Out.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnTriggerEnter(col:Collider){
+	
+	if(col.gameObject.tag == "Player")
+	{
+		Application.LoadLevel(Application.loadedLevel);
+	}
+}
